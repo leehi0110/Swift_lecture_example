a41322a0c2d1fc91cf1fa0be99a1b4aa0e7d81e1
SeungNyungLee <maple0110@naver.com>
2020-08-15 12:45:32 +0900

    2020/08/15 Swift Example DatePicker + Background Color Set
    현재 시간과 선택한 시간이 같을 경우 배경색을 바꾸는 예제

--- SwiftExample3/SwiftExample3/ViewController.swift
+++ SwiftExample3/SwiftExample3/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     let timeSelector : Selector = #selector(ViewController.updateTime)
     let interval = 1.0
     let count = 0
+    var pickerTime = ""
     
     @IBOutlet weak var lblNowTimes: UILabel!
     @IBOutlet weak var lblSelectTimes: UILabel!
@@ -29,6 +30,11 @@ class ViewController: UIViewController {
         
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
         lblSelectTimes.text = "선택한 시간: " + formatter.string(from: datePickView.date)
+        
+        let formatter2 = DateFormatter()
+        formatter2.dateFormat = "hh:mm aaa"
+        
+        pickerTime = formatter2.string(from: datePickView.date)
     }
 
     @objc func updateTime () {
@@ -38,6 +44,16 @@ class ViewController: UIViewController {
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
         
         lblNowTimes.text = "현재시간: " + formatter.string(from: date as Date)
+        
+        let formatter2 = DateFormatter()
+        formatter2.dateFormat = "hh:mm aaa"
+        
+        if formatter2.string(from: date as Date) == pickerTime {
+            view.backgroundColor = UIColor.red
+        }
+        else {
+            view.backgroundColor = UIColor.white
+        }
     }
 }
 
