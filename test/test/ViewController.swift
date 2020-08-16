bb4d214f922c2779277b709497e44ad0f74a5852
SeungNyungLee <maple0110@naver.com>
2020-08-16 17:26:07 +0900

    2020/08/16 Swift AutoLayout example
    SuperView를 타겟으로 잡아 safety를 포함하여 설계하는 example

---
+++ test/test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by allabout on 2020/08/16.
+//  Copyright © 2020 allabout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
